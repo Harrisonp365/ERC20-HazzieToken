17102c11aa3209cfb24b7dad037be1df9ee92c1a
Harrisonp365 <harrisonpowell365@gmail.com>
2020-12-31 12:24:04 +1100

    updated code as lots of the other code was depricated and also adjusted token details to suit new version of remix

--- Contract.sol
+++ Contract.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.4;
+/*pragma solidity ^0.4.4;
 
 contract Token {
     
@@ -97,5 +97,111 @@ contract HazzieToken is StandardToken {
 
         if(!_spender.call(bytes4(bytes32(sha3("recieveApproval(address, uint256, address, bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
         return true;
+    }
+}
+*/
+pragma solidity ^0.5.0;
+
+
+interface ERC20Interface {
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+ 
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract ERC20Base is ERC20Interface {
+
+    mapping (address => uint256) public _balances;
+    mapping (address => mapping (address => uint256)) public _allowances;
+    uint256 public _totalSupply;
+
+    function transfer(address _to, uint256 _value) public returns (bool success) 
+    {
+        if (_balances[msg.sender] >= _value && _value > 0) 
+        {
+            _balances[msg.sender] -= _value;
+            _balances[_to] += _value;
+            emit Transfer(msg.sender, _to, _value);
+            return true;
+        } else
+        {  return false; }
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) 
+    {
+        if (_balances[_from] >= _value && _allowances[_from][msg.sender] >= _value && _value > 0) 
+        {
+            _balances[_to] += _value;
+            _balances[_from] -= _value;
+            _allowances[_from][msg.sender] -= _value;
+            emit Transfer(_from, _to, _value);
+            return true;
+        } else 
+        { return false; }
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) 
+    {
+        return _balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) 
+    {
+        _allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) 
+    {
+      return _allowances[_owner][_spender];
+    }
+    
+    function totalSupply() public view returns (uint256 total) 
+    {
+        return _totalSupply;
+    }
+}
+
+contract HazzieToken is ERC20Base {
+
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    
+    constructor (string memory name, string memory symbol, uint8 decimals, uint256 initialSupply) public payable 
+    {
+        _name = "HazzieToken";
+        _symbol = "HPT";
+        _decimals = 0;
+        _totalSupply = 1000;
+        _balances[msg.sender] = 1000;
+    }
+
+    function name() public view returns (string memory) 
+    {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) 
+    {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) 
+    {
+        return _decimals;
     }
 }
